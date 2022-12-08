672eeac24a364559d0dc2a2a4b1cda3de84a9d57
Theo Nestmann <theo.nestmann@online.de>
2022-12-08 11:43:46 +0100

    experiments with transform for better typechecking

--- src/test/scala/pneuma/proto/Tests.scala
+++ src/test/scala/pneuma/proto/Tests.scala
@@ -9,29 +9,33 @@ class Tests extends AnyFunSuite {
     test("miscellaneous") {
         val te = \(\(1 at 0))
         val ty = (* --> *) --> (* --> *)
-        assert((te typeCheck ty) === Right(ty))
+        assert((te typeCheck ty) == Right(ty))
     }
 
     test("implicits") {
         val te = \(0 at ?)
         val ty = (* --> *) --> (* -?> *)
-        assert((te typeCheck ty).getOrElse(?) === (* --> *) --> *)
+        assert((te typeCheck ty).getOrElse(?) == ty)
     }
 
     test("modules") {
         val te = mod(
             "id" := \(0),
-            "use" := (0 ! "id") at ?
+            "use" := (0 ! "id") at ?,
+            "res" := (0 ! "use"),
         )(
             * -> *
         )
         val ty = int(
             "id" := * --> *,
-            "use" := * -?> *
+            "use" := * -?> *,
+            "res" := *,
         )(
             *
         )
-        println(((te as ty) ! "use") typeCheck *)
+        assert((te typeCheck ty) == Right(ty))
+        println(((te as ty) ! "use") typeCheck (* -?> *))
+        println(((te as ty) ! "res") typeCheck *)
     }
 
 }
